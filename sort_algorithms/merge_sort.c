cf61c9b6988feb2ddf5ee9490a48c27bacec8f0c
Italo Pinheiro <pinheiroitalo2004@gmail.com>
2024-07-11 09:28:25 -0400

    mudanca no merge sort

--- sort_algorithms/merge_sort.c
+++ sort_algorithms/merge_sort.c
@@ -6,37 +6,37 @@
 void merge(int arr[], int l, int m, int r)
 {
    int i, j, k;
-   int n1 = m - l + 1;
-   int n2 = r - m;
+   int n1 = m - l + 1; // Tamanho do primeiro subarray
+   int n2 = r - m;     // Tamanho do segundo subarray
+
    // Cria arrays temporarios
    int L[n1], R[n2];
+   // Preenche o array L com todos os elementos até a posicao (n1 - 1)
    for (i = 0; i < n1; i++)
       L[i] = arr[l + i];
-   for (j = 0; j < n2; j++)
-      R[j] = arr[m + 1 + j];
-
-   // Mescla os arrays temporarios L[] e R[]
-   for (i = 0; i < n1; i++)
-      L[i] = arr[l + i];
+   // Preenche o array R com todos os elementos até a posicao (n2 - 1)
    for (j = 0; j < n2; j++)
       R[j] = arr[m + 1 + j];
 
    // Mescla os arrays temporarios de volta para arr[l ... r]
    i = 0; // Indice inicial do primeiro subarray
-   j = 0;
-   k = l; // Indice inicial do array mesclado
+   j = 0; // Indice inicial do array mesclado
+   k = l; // Indice do array de entrada
    while (i < n1 && j < n2)
    {
+      // O array L precisa ter todos os elementos menores
       if (L[i] <= R[j])
       {
          arr[k] = L[i];
          i++;
       }
+      // Enquanto os maiores estão no array R
       else
       {
          arr[k] = R[j];
          j++;
       }
+      // Itera o indice do array principal
       k++;
    }
 
